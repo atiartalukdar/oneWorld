5cd37f1c7f34be5ac1843083620013e35030fafd
Atiar Talukdar <atiartalukdar@gmail.com>
2015-12-22 03:25:37 +0600

    layout 3

--- app/src/main/java/com/oneworld/talukdar/oneworld/MainActivity.java
+++ app/src/main/java/com/oneworld/talukdar/oneworld/MainActivity.java
@@ -1,9 +1,11 @@
 package com.oneworld.talukdar.oneworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void countryBtn(View V){
+        Intent intent =new Intent(this,CountryDetails.class);
+        startActivity(intent);
+    }
+
+    public void continentBtn(View V){
+        Intent intent =new Intent(this,CountryDetails.class);
+        startActivity(intent);
     }
 
 
