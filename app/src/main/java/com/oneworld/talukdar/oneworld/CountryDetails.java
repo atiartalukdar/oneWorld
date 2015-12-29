426410e23ba8febd430349bc2bc1399572de3e27
Atiar Talukdar <atiartalukdar@gmail.com>
2015-12-29 07:03:31 +0600

    taking image name from database

--- app/src/main/java/com/oneworld/talukdar/oneworld/CountryDetails.java
+++ app/src/main/java/com/oneworld/talukdar/oneworld/CountryDetails.java
@@ -1,13 +1,125 @@
 package com.oneworld.talukdar.oneworld;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import customAdepter.CountryItems;
 
 public class CountryDetails extends AppCompatActivity {
+    private SQLiteDatabase database;
+private String[] elements;
+    //For Continent
+    private static final String TABLE_NAME = "Continent";
+    private static final String UID = "_id";
+    private static final String CONTINENT = "Continent";
+    private static final String AREA_KM = "Area_Km";
+    private static final String POPULATION = "Population";
+    private static final String DENSITY_KM = "Density_People_Per_Km";
+    private static final String DENSITY_MILE = "Density_People_Per_Mile";
+    private static final String MOST_POPULAS_CITY = "Most_Populas_city";
 
+    //For Country
+    private static final String TABLE_NAME_COUNTRY = "Country";
+    private static final String UID_C = "_id";
+    private static final String CONTINENT_C = "Continent";
+    private static final String COUNTRY_C = "Country";
+    private static final String CAPITAL_C = "Capital";
+    private static final String CURRENCY_C = "Currency";
+    private static final String REGION_C = "Region";
+    private static final String POPULATION_C = "Population";
+    private static final String AREA_KM_C = "Area_sq_mi";
+    private static final String DENSITY_MILE_C = "Pop_Density_per_sq_mile";
+    private static final String GBP_C = "GDP";
+    private static final String LITERACY_C = "Literacy";
+    private static final String PHONE_CODE_C = "PhoneCode";
+
+    ImageView imageView;
+    TextView countryTV;
+    String data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_details);
+        countryTV = (TextView) findViewById(R.id.CountryNameDetails);
+
+        Intent intent = getIntent();
+        data = intent.getExtras().getString("key1");
+
+        //In elements[0] will contain the clicked items position, & elements[1] will contain the database table name;
+        elements = data.split(" ");
+
+        getAllData();
     }
-}
+
+
+
+
+
+        public void getAllData(){
+        // SELECT _id,Name,Password from Continent //Table
+        Cursor cursor=null;
+        String[] columns_Continent = {UID,CONTINENT,AREA_KM,POPULATION,DENSITY_KM,DENSITY_MILE,MOST_POPULAS_CITY};
+        String[] columns_country = {UID_C,CONTINENT_C,COUNTRY_C,CAPITAL_C,CURRENCY_C,REGION_C,POPULATION_C,
+                AREA_KM_C,DENSITY_MILE_C,GBP_C,LITERACY_C,PHONE_CODE_C};
+
+        if(elements[1].trim().equals("1")){
+           Toast.makeText(getApplicationContext(), "One", Toast.LENGTH_LONG).show();
+            cursor = database.query(TABLE_NAME, columns_Continent, UID+" = '"+Integer.parseInt(elements[0])+"'", null, null, null, null);
+            while(cursor.moveToNext()){
+                Context context=getApplicationContext();
+
+                int Index_Continent = cursor.getColumnIndex(CONTINENT);
+                int Index_POPULATION = cursor.getColumnIndex(POPULATION);
+
+                int cid = cursor.getInt(0);
+                String name = cursor.getString(Index_Continent);
+                String population = cursor.getString(Index_POPULATION);
+
+                String nameLowerCase = name.toLowerCase().replaceAll("\\s+", "");
+                int id = context.getResources().getIdentifier(nameLowerCase, "drawable", context.getPackageName());
+                //Drawable d = getResources().getDrawable(id);
+
+                countryTV.setText(name);
+            }
+            cursor.close();
+            database.close();
+        }
+    /*    if(data.equals("2")){
+            Toast.makeText(getApplicationContext(),"Two",Toast.LENGTH_LONG).show();
+            cursor = database.query(TABLE_NAME_COUNTRY, columns_country, null, null, null, null, null);
+            while(cursor.moveToNext()){
+                Context context=getApplicationContext();
+
+                int country_Index = cursor.getColumnIndex(COUNTRY_C);
+                int capital_Index = cursor.getColumnIndex(CAPITAL_C);
+
+                int cid = cursor.getInt(0);
+                String name = cursor.getString(country_Index);
+                String population = cursor.getString(capital_Index);
+
+                String nameLowerCase = name.toLowerCase().replaceAll("\\s+", "");
+                int id = context.getResources().getIdentifier(nameLowerCase, "drawable", context.getPackageName());
+                //Drawable d = getResources().getDrawable(id);
+
+                CountryItems details = new CountryItems(name,population,id);
+                countryItems.add(details);
+
+                buffer.append(cid + " " + name + " " + population + "\n");
+            }
+            cursor.close();
+            database.close();
+        }*/
+
+
+    }
+
+
+    }
+
--- app/src/main/java/com/oneworld/talukdar/oneworld/listView.java
+++ app/src/main/java/com/oneworld/talukdar/oneworld/listView.java
@@ -29,6 +29,7 @@ public class listView extends AppCompatActivity {
     CountryAdepter countryAdepter;
     ListView listView;
     private SQLiteDatabase database;
+    String data;
 
 
     private static final String DB_NAME = "Country_Details.db";
@@ -43,6 +44,21 @@ public class listView extends AppCompatActivity {
     private static final String DENSITY_MILE = "Density_People_Per_Mile";
     private static final String MOST_POPULAS_CITY = "Most_Populas_city";
 
+    //For Country
+    private static final String TABLE_NAME_COUNTRY = "Country";
+    private static final String UID_C = "_id";
+    private static final String CONTINENT_C = "Continent";
+    private static final String COUNTRY_C = "Country";
+    private static final String CAPITAL_C = "Capital";
+    private static final String CURRENCY_C = "Currency";
+    private static final String REGION_C = "Region";
+    private static final String POPULATION_C = "Population";
+    private static final String AREA_KM_C = "Area_sq_mi";
+    private static final String DENSITY_MILE_C = "Pop_Density_per_sq_mile";
+    private static final String GBP_C = "GDP";
+    private static final String LITERACY_C = "Literacy";
+    private static final String PHONE_CODE_C = "PhoneCode";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +68,9 @@ public class listView extends AppCompatActivity {
         ExternalDbOpenHelper dbOpenHelper = new ExternalDbOpenHelper(this, DB_NAME);
         database = dbOpenHelper.openDataBase();
 
+        Intent intent = getIntent();
+        data = intent.getExtras().getString("key");
+
         String s =getAllData();
         Message.message(this,s);
 
@@ -59,80 +78,66 @@ public class listView extends AppCompatActivity {
         countryItems.add(details);*/
        // fillList();
         setUpList();
+    }
 
-
-
-
-
-        Intent intent = getIntent();
-        String data = intent.getExtras().getString("key");
-
-        if(data.equals("1")){
+ public String getAllData(){
+        // SELECT _id,Name,Password from Continent //Table
+        Cursor cursor=null;
+        String[] columns_Continent = {UID,CONTINENT,AREA_KM,POPULATION,DENSITY_KM,DENSITY_MILE,MOST_POPULAS_CITY};
+        String[] columns_country = {UID_C,CONTINENT_C,COUNTRY_C,CAPITAL_C,CURRENCY_C,REGION_C,POPULATION_C,
+             AREA_KM_C,DENSITY_MILE_C,GBP_C,LITERACY_C,PHONE_CODE_C};
+     StringBuffer buffer = new StringBuffer();
+     if(data.equals("1")){
             Toast.makeText(getApplicationContext(),"One",Toast.LENGTH_LONG).show();
+            cursor = database.query(TABLE_NAME, columns_Continent, null, null, null, null, null);
+         while(cursor.moveToNext()){
+             Context context=getApplicationContext();
+
+             int Index_Continent = cursor.getColumnIndex(CONTINENT);
+             int Index_POPULATION = cursor.getColumnIndex(POPULATION);
+
+             int cid = cursor.getInt(0);
+             String name = cursor.getString(Index_Continent);
+             String population = cursor.getString(Index_POPULATION);
+
+             String nameLowerCase = name.toLowerCase().replaceAll("\\s+", "");
+             int id = context.getResources().getIdentifier(nameLowerCase, "drawable", context.getPackageName());
+             //Drawable d = getResources().getDrawable(id);
+
+             CountryItems details = new CountryItems(name,population,id);
+             countryItems.add(details);
+
+             buffer.append(cid+" "+name+" "+population+ "\n");
+         }
+         cursor.close();
+         database.close();
         }
         if(data.equals("2")){
             Toast.makeText(getApplicationContext(),"Two",Toast.LENGTH_LONG).show();
-        }
+            cursor = database.query(TABLE_NAME_COUNTRY, columns_country, null, null, null, null, null);
+            while(cursor.moveToNext()){
+                Context context=getApplicationContext();
 
-    }
+                int country_Index = cursor.getColumnIndex(COUNTRY_C);
+                int capital_Index = cursor.getColumnIndex(CAPITAL_C);
 
+                int cid = cursor.getInt(0);
+                String name = cursor.getString(country_Index);
+                String population = cursor.getString(capital_Index);
 
-/*    private void fillList() {
-        //friends = new ArrayList<String>();  which was countryItems
-        //String[] columns = {UID,CONTINENT,AREA_KM,POPULATION,DENSITY_KM,DENSITY_MILE,MOST_POPULAS_CITY};
-        Cursor continentNameCorsor = database.query(TABLE_NAME,new String[]{CONTINENT},null, null, null, null,null);
-        Log.i("listView", "C1");
-        Cursor populationCorsor = database.query(TABLE_NAME,new String[]{POPULATION},null, null, null, null,null);
-        Log.i("listView", "2");
-        continentNameCorsor.moveToFirst();
-        populationCorsor.moveToFirst();
+                String nameLowerCase = name.toLowerCase().replaceAll("\\s+", "");
+                int id = context.getResources().getIdentifier(nameLowerCase, "drawable", context.getPackageName());
+                //Drawable d = getResources().getDrawable(id);
 
-        int continent_Index = continentNameCorsor.getColumnIndex(CONTINENT);
-        int population_Index = populationCorsor.getColumnIndex(POPULATION);
-
-        if(!continentNameCorsor.isAfterLast()) {
-            do {
-                String ContinentName = continentNameCorsor.getString(continent_Index);
-                String PopulationNumber = populationCorsor.getString(population_Index);
-                Log.i("listView", "Do loop");
-                CountryItems details = new CountryItems(ContinentName,PopulationNumber);
+                CountryItems details = new CountryItems(name,population,id);
                 countryItems.add(details);
 
-            } while (continentNameCorsor.moveToNext());
+                buffer.append(cid + " " + name + " " + population + "\n");
+            }
+            cursor.close();
+            database.close();
         }
-        continentNameCorsor.close();
-        populationCorsor.close();
-    }*/
 
-    public String getAllData(){
-
-
-
-        // SELECT _id,Name,Password from Continent //Table
-        String[] columns = {UID,CONTINENT,AREA_KM,POPULATION,DENSITY_KM,DENSITY_MILE,MOST_POPULAS_CITY};
-        Cursor cursor = database.query(TABLE_NAME, columns, null, null, null, null, null);
-
-        StringBuffer buffer = new StringBuffer();
-        while(cursor.moveToNext()){
-            Context context=getApplicationContext();
-
-            int continent_Index = cursor.getColumnIndex(CONTINENT);
-            int population_Index = cursor.getColumnIndex(MOST_POPULAS_CITY);
-
-            int cid = cursor.getInt(0);
-            String name = cursor.getString(continent_Index);
-            String nameLowerCase = name.toLowerCase().replaceAll("\\s+","");
-
-            String pass = cursor.getString(population_Index);
-            int id = context.getResources().getIdentifier(nameLowerCase, "drawable", context.getPackageName());
-            //Drawable d = getResources().getDrawable(id);
-
-            CountryItems details = new CountryItems(name,pass,id);
-            countryItems.add(details);
-
-            buffer.append(cid+" "+nameLowerCase+" "+pass+ "\n");
-        }
-        cursor.close();
         return buffer.toString();
     }
 
@@ -147,11 +152,11 @@ public class listView extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-
+                Intent intent =new Intent(listView.this,CountryDetails.class);
+                intent.putExtra("key1", position+1+" "+data);
+                startActivity(intent);
                 Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-
 }
